41cf1adb0f4d76feea06bc55d8f8196e47cf5ba9
0fajarpurnama0 <fajar.purnama@yahoo.com>
2017-10-14 00:21:45 +0900

    Changes to be committed: 	deleted:    moodle/Taking The First Step/backup.mbz 	deleted:    moodle/Taking The First Step/backup.mbz.backup 	deleted:    moodle/Taking The First Step/backup.mbz.delta 	deleted:    moodle/Taking The First Step/backup.mbz.sig 	deleted:    moodle/Taking The First Step/md5sum 	deleted:    moodle/Taking The First Step/varset.php

--- moodle/Taking The First Step/varset.php
+++
@@ -1,6 +0,0 @@
-
-    <?php
-    $url ="moodle-experiment-fajarpurnama.c9users.io:80/sync/file-synchronizer-online-course-sharing/manual/mine/" ; 
-    $content_url = "moodle-experiment-fajarpurnama.c9users.io:80/sync/file-synchronizer-online-course-sharing/manual/mine/backup.mbz" ;
-    ?>
-    
